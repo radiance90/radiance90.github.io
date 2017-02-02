7b462292c2e28c47d6f5ea47e7ec54a56b07389f
radiance90 <henry.j.yu@hotmail.com>
2017-02-03 03:16:30 +1100

    add animation to navbar and header

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ app.config(function($routeProvider,$locationProvider) {
 	$routeProvider.when('/', {
 		controller: 'HomeController',
 		templateUrl: 'views/home.html'
-	}).when('/about', {
-		controller: 'AboutController',
-		templateUrl: 'views/about.html'
+	}).when('/profile', {
+		controller: 'ProfileController',
+		templateUrl: 'views/profile.html'
 	}).when('/contact', {
 		controller: 'ContactController',
 		templateUrl: 'views/contact.html'
--- js/script.js
+++ js/script.js
@@ -8,20 +8,28 @@ $(document).ready(function() {
     $('.navbar-custom .nav li a').click(function() {
         $('.navbar-custom .nav li').removeClass('active');
         $(this).parent().addClass('active');
-        if ($(this).text()=='About') {
+        if ($(this).text()=='Profile') {
             $('#mainNav').removeClass('navbar-transparent');
+            $('#header').addClass('section-hide');
+            $('#about').addClass('section-hide');
         }
         if ($(this).text()=='Contact') {
             $('#mainNav').removeClass('navbar-transparent');
+            $('#header').addClass('section-hide');
+            $('#about').addClass('section-hide');
         }
         if ($(this).text()=='Home') {
             $('#mainNav').addClass('navbar-transparent');
+            $('#header').removeClass('section-hide');
+            $('#about').removeClass('section-hide');
         }
      });
     $('.navbar-brand').click(function() {
         $('.navbar-custom .nav li').removeClass('active');
         $('.navbar-nav li:first-child').addClass('active');
         $('#mainNav').addClass('navbar-transparent');
+        $('#header').removeClass('section-hide');
+        $('#about').removeClass('section-hide');
     });
     // navbar offset
     $('#mainNav').affix({
@@ -29,5 +37,29 @@ $(document).ready(function() {
             top: 100
         }
     })
+
+    
+
+    $("header a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
     
 });
