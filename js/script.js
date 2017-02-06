45adb0c6dff1dc888d861dffbbecbf6d015a0c9e
radiance90 <henry.j.yu@hotmail.com>
2017-02-06 22:47:12 +1100

    update index.html add contact

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li.link a').click(function(){ 
             $('.navbar-toggle:visible').click();
     });
 
